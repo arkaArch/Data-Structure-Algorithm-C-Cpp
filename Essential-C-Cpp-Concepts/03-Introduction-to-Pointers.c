ea211f0ca0a8a980b07adc2459d98bb4f05e6b52
arka <arkakar1313@gmail.com>
2025-12-20 20:14:29 +0530

    Initialize

---
+++ Essential-C-Cpp-Concepts/03-Introduction-to-Pointers.c
@@ -0,0 +1,4 @@
+/* A program can access code and stack section of memory directly but it can't
+access heap section of memory(like ssd, hdd), to access this we need pointers.
+Also to access outside resources like network, keyboard etc. we nedd pointers.
+*/
