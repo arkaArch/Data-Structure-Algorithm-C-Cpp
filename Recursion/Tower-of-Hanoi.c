ceff611a109a1ce6abae82a817ef755015ca1cdd
arka <arkakar1313@gmail.com>
2025-12-29 22:13:36 +0530

    Added the disk number

--- Recursion/Tower-of-Hanoi.c
+++ Recursion/Tower-of-Hanoi.c
@@ -5,7 +5,7 @@ void TOH(int n, char A, char B, char C) {
     /* Move n-1 disks from A to B not using C */
     TOH(n - 1, A, C, B);
     /* Move one disk from A to C not using B => TOH(1, A, B, C); */
-    printf("Move a disk from %c to %c not using %c\n", A, B, C);
+    printf("Move disk %d from %c to %c not using %c\n", n, A, B, C);
     /* Move n-1 disks from B to C not using A */
     TOH(n - 1, B, A, C);
   }
